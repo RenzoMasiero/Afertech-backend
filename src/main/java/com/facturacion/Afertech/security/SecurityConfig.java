0dc487ea2bc6b866e405740ccc9f347c1c3e827d
RenzoMasiero88 <renzo.masiero.dev@gmail.com>
2026-01-11 19:37:51 -0300

    Revert "arrglo cros"
    This reverts commit 2b9274c0e9b9627a39c6e0b09ce0490823b489a3.

--- src/main/java/com/facturacion/Afertech/security/SecurityConfig.java
+++ src/main/java/com/facturacion/Afertech/security/SecurityConfig.java
@@ -83,10 +83,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(
-                List.of(
-                        "http://localhost:5173",
-                        "https://afertech-frontend-production.up.railway.app"
-                )
+                List.of("http://localhost:5173")
         );
         config.setAllowedMethods(
                 List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
