2b9274c0e9b9627a39c6e0b09ce0490823b489a3
RenzoMasiero88 <renzo.masiero.dev@gmail.com>
2026-01-11 15:42:51 -0300

    arrglo cros

--- src/main/java/com/facturacion/Afertech/security/SecurityConfig.java
+++ src/main/java/com/facturacion/Afertech/security/SecurityConfig.java
@@ -83,7 +83,10 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of(
+                        "http://localhost:5173",
+                        "https://afertech-frontend-production.up.railway.app"
+                )
         );
         config.setAllowedMethods(
                 List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
